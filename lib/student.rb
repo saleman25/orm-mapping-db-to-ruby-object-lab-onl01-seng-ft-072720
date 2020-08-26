ecd614d16fbb5f0df5811394784aacc24a14ab0d
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 21:19:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,11 @@ class Student
   attr_accessor :id, :name, :grade
 
 def self.new_from_db(row)
-  new_song = self.new  # self.new is the same as running Song.new
-  new_song.id = row[0]
-  new_song.name =  row[1]
-  new_song.length = row[2]
-  new_song 
+  new_student = self.new
+  new_student.id = row[0]
+  new_student.name =  row[1]
+  new_student.grade = row[2]
+  new_student 
 end
 
   def self.all
