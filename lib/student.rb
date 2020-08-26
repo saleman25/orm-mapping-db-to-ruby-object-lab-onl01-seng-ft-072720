493d3e984e7ad10d9d82d23422a22dac552ff111
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 21:42:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,47 +4,51 @@ class Student
 
 
 def self.new_from_db(row)
-  new_student = self.new
-  new_student.id = row[0]
-  new_student.name =  row[1]
-  new_student.grade = row[2]
-  new_student 
-end
+    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
 
-def self.all
+  def self.all
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
     sql = <<-SQL
       SELECT *
       FROM students
     SQL
- 
+
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
+    end
   end
-end
 
-def self.find_by_name(name)
-  sql = <<-SQL
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
       SELECT *
       FROM students
       WHERE name = ?
       LIMIT 1
     SQL
- 
-    DB[:conn].execute(sql, name).map do |row|
+
+    DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
   end
 
-  
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -59,9 +63,21 @@ def self.find_by_name(name)
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
+
     DB[:conn].execute(sql)
   end
-end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
 def self.students_below_12th_grade
     sql = <<-SQL
