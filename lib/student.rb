8821121e6ddd6f0200c44dc618262b2f59bafa29
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 21:18:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,13 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
-    # create a new Student object given a row from the database
-  end
+def self.new_from_db(row)
+  new_song = self.new  # self.new is the same as running Song.new
+  new_song.id = row[0]
+  new_song.name =  row[1]
+  new_song.length = row[2]
+  new_song 
+end
 
   def self.all
     # retrieve all the rows from the "Students" database
